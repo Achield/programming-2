c1e147315f502ed19cf99aa8bcde644558555510
Achield <achieldoriginal@gmail.com>
2024-09-11 16:45:53 -0400

    random stuff

--- Assets/WASD.cs
+++ Assets/WASD.cs
@@ -8,19 +8,27 @@ public class WASD : MonoBehaviour
     public float accel = 10f;   // A public variable to multiply direction speed by, and can be controlled in the editor because public
     public float horAccel = 0.5f;
     public float vertAccel = 0.5f;
+    private Rigidbody2D rb;
+    public float jumpHeight;
+    public bool canJump;
+    public LayerMask Collision_Mask;
 
     public float collectedScore = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        jumpHeight = 500f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jumpHeight));
+        }
     }
 
     // FixedUpdate is called every physics update
@@ -42,7 +50,7 @@ public class WASD : MonoBehaviour
         float y = Input.GetAxis("Vertical");     // Uses Unity's default vertical axis inputs (W and S or arrow keys up and down) to get a value between -1 to 1
         float x = Input.GetAxis("Horizontal");   // Uses Unity's default horizontal axis inputs (A and S or arrow keys left and right) to get a value between -1 to 1
 
-        Vector3 myDir = new Vector3(x, y, 0);   // Constructs the vector based off the vertical and horizontal axis inputs
+        Vector3 myDir = new Vector3(x, 0, 0);   // Constructs the vector based off the vertical and horizontal axis inputs
         return myDir;     // Returns that value
     }
 
@@ -56,4 +64,30 @@ public class WASD : MonoBehaviour
             collectedScore++;    // Adds +1 to collectedScore on every collision
         }
     }
+
+    public void canJumpCheck()
+    {
+        Vector2 RayCastDownLeftPosition = new Vector2(transform.position.x - 0.32f, transform.position.y);
+
+        Vector2 RayCastDownRightPosition = new Vector2(transform.position.x + 0.32f, transform.position.y);
+
+        Vector2 RayCastDownDirection = Vector2.down;
+
+        float RayCastDownDistance = 0.34f;
+
+        RaycastHit2D HitLeft = Physics2D.Raycast(RayCastDownLeftPosition, RayCastDownDirection, RayCastDownDistance, Collision_Mask);
+
+        RaycastHit2D HitRight = Physics2D.Raycast(RayCastDownRightPosition, RayCastDownDirection, RayCastDownDistance, Collision_Mask);
+
+        if (HitLeft.collider != null || HitRight.collider != null)
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+    }
+
+
 }
