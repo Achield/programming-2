6d16e187cdc1a90a36d4593e4f56115bc1ee9ff7
Achield <achieldoriginal@gmail.com>
2024-09-25 16:50:23 -0400

    added a kill block

--- Assets/eyeballTracking.cs
+++ Assets/eyeballTracking.cs
@@ -16,13 +16,29 @@ public class eyeballTracking : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Calculating the distance between the object and the player (this is in world space)
         Vector3 delta = myTarget.transform.position - transform.position;
-        Debug.DrawRay(transform.position, delta, Color.yellow);
-        Debug.DrawRay(transform.position, Vector3.up, Color.cyan);
 
-        float angleDelta = Vector3.Angle(delta, Vector3.up);
+        // Debug checking the distance from eyeball to player
+        Debug.DrawRay(transform.position, delta, Color.yellow);
+
+        // Define localUp and localForward
+        Vector3 localUp = transform.TransformDirection(Vector3.up);
+        Vector3 localForward = transform.TransformDirection(Vector3.forward);
+
+        // Debug showing the up vector of the eyeball (this line needs fixing)
+        Debug.DrawRay(transform.position, localUp, Color.cyan);
+        Debug.DrawRay(transform.position, localForward, Color.red);
+
+        // angleDelta find the angle between two vectors in this case
+        // The local up of the eyeball (pupil) and the vector from player to eyeball
+        float angleDelta = Vector3.Angle(delta, localUp);
         Debug.Log("current angle: " + angleDelta);
 
-        transform.Rotate(Vector3.forward, angleDelta);
+        // Rotate by the difference
+        if (angleDelta > 0.5)
+        {
+            transform.Rotate(Vector3.forward, angleDelta);
+        }
     }
 }
--- Assets/gameManager.cs
+++ Assets/gameManager.cs
@@ -7,19 +7,26 @@ public class gameManager : MonoBehaviour
 {
     public float myTimer = 0f;    // Public time passed variable
     public float myFixedTimer = 0f;   // Public fixed update time
-    public float spawnInterval = 0.5f;
-    public float spawnTimer = 0f;
+
     public GameObject myCollectible;
     public GameObject myPlayer;
     public TextMeshProUGUI myScore;
     float playerScore = 0f;
     WASD playerScript;
 
+    [Header("Collectible spawn variables")]
+    public float spawnInterval = 0.5f;
+    public float spawnTimer = 0f;
+    public Vector4 spawnBox = Vector4.zero;
+
     // Start is called before the first frame update
     void Start()
     {
         myPlayer = GameObject.FindGameObjectWithTag("Player");
         playerScript = myPlayer.GetComponent<WASD>();
+
+        // Invoke repeating is a method that calls a function and runs it every X seconds, with a Y second delay to start
+        InvokeRepeating("CollectibleSpawn", spawnInterval / 2, spawnInterval);
     }
 
     // Update is called once per frame
@@ -27,13 +34,13 @@ public class gameManager : MonoBehaviour
     {
         myTimer += Time.deltaTime;      // Add time passed between frames
 
-        spawnTimer += Time.deltaTime;       // Track enemy spawn time
-        if (spawnTimer >= spawnInterval)     // Once the interval is hit, trigger an enemy spawn and reset timer
-        {
-            spawnTimer = 0f;
-            Instantiate(myCollectible);
-            // Debug.Log("Collectible spawned");
-        }
+        //spawnTimer += Time.deltaTime;       // Track enemy spawn time
+        //if (spawnTimer >= spawnInterval)     // Once the interval is hit, trigger an enemy spawn and reset timer
+        //{
+        //    spawnTimer = 0f;
+        //    Instantiate(myCollectible);
+        //    // Debug.Log("Collectible spawned");
+        //}
         // Because gameManager has an explicit connection to the player we can reference the player components including WASD.cs to find score
         playerScore = playerScript.collectedScore;
         myScore.text = playerScore.ToString();
@@ -43,4 +50,13 @@ public class gameManager : MonoBehaviour
     {
         myFixedTimer += Time.fixedDeltaTime;     // Add time passed between each physics frame
     }
+
+    //void CollectibleSpawn()
+    //{
+    //    // Generate a random XY coordinate to spawn the collectible at
+    //    Vector3 targetPos = new Vector3(Random.Range(spawnBox.x, spawnBox.y), Random.Range(spawnBox.z, spawnBox.w), 0);
+    //    // Instantiate can also be called with a vector3 for target spawn location, and a quaternion for rotation (just use Quaternion.identity)
+    //    Instantiate(myCollectible, targetPos, Quaternion.identity);
+    //    Debug.Log("collectible spawn");
+    //}
 }
